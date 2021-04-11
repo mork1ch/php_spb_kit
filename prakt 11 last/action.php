e117a2ee0f3914e91607f4dee99682c76b9057c5
mork1ch <mork1cher@gmail.com>
2021-04-12 00:04:08 +0300

    Я сделал рабочую 11 лабу

--- prakt 11 last/action.php
+++ prakt 11 last/action.php
@@ -5,16 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Проверка регистрации</title>
+    <title>11</title>
 </head>
 <body>
 
     <?php
 
+        // class Forma{
+
+        // }
+        // $forma = new Forma();
+
         class Form{
 
-            //$data = []; //Приходится коментировать, потому что denver не воспринимает обьявление массива. Хз почему
-            //$osibki = [];
+            public $data = [];
+            public $osibki = [];
 
             function __construct() {
                 $this->data[familiy]    = ($_POST['familiy']);
@@ -25,13 +30,13 @@
                 $this->data[group]      = ($_POST['group']);
                 $this->data[country]    = ($_POST['country']);
                 $this->data[adress]     = ($_POST['adress']);
-                $this->data[tel]        = "+7".($_POST['tel']);
+                $this->data[tel]        = "7".($_POST['tel']);
                 $this->data[email]      = ($_POST['email']);
                 $this->data[hobbi]      = ($_POST['hobbi']);
                 $this->data[dop_inf]    = ($_POST['dop_inf']);
             }
 
-            function Proverka(){
+            function Proverka() {
                 $data = $this->data;
 
                 if(empty($data[familiy])){
@@ -58,25 +63,11 @@
                     $osibki[] = "В отчестве присутствует пробел";
                 }
 
-                if(empty($data[pol])){
-                    $data[pol] = "не указанно";
-                }
-
-                if(empty($data[birthday])){
-                    $data[birthday] = "не указанно";
-                }
-
-                if(empty($data[country])){
-                    $data[country] = "не указанно";
-                }
-
                 if(empty($data[adress])){
                     $data[adress] = "не указанно";
                 }elseif(preg_match("/^[0-1A-Za-zА-Яа-я]*\S*$/", $data[adress])){
-                    $data[adress] = "не указанно";
+                    $data[adress] = "не указанно)";
                 }
-                
-                
 
                 if(empty($data[tel])){
                     $osibki[] = "Номер не указан";
@@ -91,71 +82,162 @@
                 }elseif(!preg_match("/^[0-1A-Za-zА-Яа-я]*\S*$/", $data[email])){
                     $osibki[] = "Мейл введен пробел";
                 }
+
+                $GLOBALS["osibki"] = $osibki;
+
+
+            }
+
+            function osibki(){
+                $osibki = $this->osibki;
                 
+                echo "<h1>Были выявлены ошибки</h1>";
+                echo "</br>";
+
+                $data = $this->data;
+
+                if(empty($data[familiy])){
+                    $osibki[] = "Не указана фамилия";
+                }elseif(!preg_match("/[A-Za-zА-Яа-я]*$/", $data[familiy])){
+                    $osibki[] = "В фамилии присутствуют цифры";
+                }elseif(!preg_match("/^[A-Za-zА-Яа-я]*\S*$/", $data[familiy])){
+                    $osibki[] = "В фамилии присутствует пробел";
+                }
+
+                if(empty($data[name])){
+                    $osibki[] = "Не указано имя";
+                }elseif(!preg_match("/[A-Za-zА-Яа-я]*$/", $data[name])){
+                    $osibki[] = "В имени присутствуют цифры";
+                }elseif(!preg_match("/^[A-Za-zА-Яа-я]*\S*$/", $data[name])){
+                    $osibki[] = "В имени присутствует пробел";
+                }
+
+                if(empty($data[otch])){
+                    $osibki[] = "Не указано отчество";
+                }elseif(!preg_match("/[A-Za-zА-Яа-я]*$/", $data[otch])){
+                    $osibki[] = "В отчестве присутствуют цифры";
+                }elseif(!preg_match("/^[A-Za-zА-Яа-я]*\S*$/", $data[otch])){
+                    $osibki[] = "В отчестве присутствует пробел";
+                }
+
+                if(empty($data[adress])){
+                    $data[adress] = "не указанно";
+                }elseif(preg_match("/^[0-1A-Za-zА-Яа-я]*\S*$/", $data[adress])){
+                    $data[adress] = "не указанно)";
+                }
+
+                if(empty($data[tel])){
+                    $osibki[] = "Номер не указан";
+                }elseif(!preg_match("/^[0-9][0-9]{10}$/", $data[tel])){
+                    $osibki[] = "Телефон задан в неверном формате(напишите 10цифр после '+7')";
+                }
+
+                if(empty($data[email])){
+                    $osibki[] = "email не указан";
+                }elseif(!preg_match("/[0-9A-Za-z]*[@gmail.com|@mail.ru]$/", $data[email])){
+                    $osibki[] = "Мейл введен неверно";
+                }elseif(!preg_match("/^[0-1A-Za-zА-Яа-я]*\S*$/", $data[email])){
+                    $osibki[] = "Мейл введен пробел";
+                }
+
+
+                foreach ($osibki as $value){
+                    echo "$value</br>";
+                }
+
+                echo "</br>";
+                echo "<a href='index.html'>Перезаполнить таблицу</a>";
             }
 
             function Vivod() {
-                if(empty($osibki)){
-                    $data = $this->data;
-                    
-                    if (!empty($_POST['hobbi'])){
-                        $hobbi = implode(',', $data[hobbi]);
-                        $data[hobbi] = $hobbi;
-                    }else{
-                        $data[hobbi] = 'Не указанно';
-                    }
+                $data = $this->data;
 
-                    echo "<h1>Проверьте форму</h1>";
-                    echo "<p>Все правильно ?</p>";
-
-                    echo "</br>";
-
-                    echo "ФИО: $data[familiy] $data[name] $data[otch]</br>";
-                    echo "Дата рождения: $data[birthday]</br>";
-                    echo "Пол: $data[pol]</br>";
-                    echo "Группа: $data[group]</br>";
-                    echo "Страна: $data[country]</br>";
-                    echo "Адрес: $data[adress]</br>";
-                    echo "Телефон: $data[tel]</br>";
-                    echo "E-mail: $data[email]</br>";
-                    echo "Увлечения: $hobbi[]</br>";
-                    echo "Дополнительно: $data[dop_inf]</br>";
-
-                    echo "</br>";
-                    
-                }else{
-                    echo "<h1>Были выявлены ошибки</h1>";
-                    echo "</br>";
-
-                    foreach ($osibki as $value) echo "$value</br>";
-
-                    echo "</br>";
-                    echo "<a href='index.html'>Перезаполнить таблицу</a>";
+                if(empty($data[birthday])){
+                    $data[birthday] = "не указанно";
                 }
+
+                if(empty($data[country])){
+                    $data[country] = "не указанно";
+                }
+
+                if (!empty($_POST['hobbi'])){
+                    $hobbi = implode(',', $data[hobbi]);
+                    $data[hobbi] = $hobbi;
+                }else{
+                    $data[hobbi] = 'Не указанно';
+                }
+
+                if (empty($_POST[dop_inf])){
+                    $data[dop_inf] = 'Не указанно';
+                }
+
+                echo "<h1>Проверьте форму</h1>";
+                echo "<p>Все правильно ?</p>";
+
+                echo "</br>";
+
+                echo "ФИО: $data[familiy] $data[name] $data[otch]</br>";
+                echo "Дата рождения: $data[birthday]</br>";
+                echo "Пол: $data[pol]</br>";
+                echo "Группа: $data[group]</br>";
+                echo "Страна: $data[country]</br>";
+                echo "Адрес: $data[adress]</br>";
+                echo "Телефон: $data[tel]</br>";
+                echo "E-mail: $data[email]</br>";
+                echo "Увлечения: $data[hobbi]</br>";
+                echo "Дополнительно: $data[dop_inf]</br>";
+
+                echo "</br>";
+                
             }
 
             function Zapis(){
                 $data = $this->data;
-                
-                if(empty($osibki)) { 
-                    $fp = fopen ('info_pers/bd.txt', 'a+');
-                    foreach ($data as $key => $values) {
-                        fwrite ($fp, "$data[$key] ");
-                    }
 
-                    fwrite ($fp, PHP_EOL);
-                    fclose ($fp);
-                    echo "Данные записаны!";
-                }else{ echo "<h1>Были выявлены ошибки</h1>"; }
+                if(empty($data[birthday])){
+                    $data[birthday] = "не указанно";
+                }
+
+                if(empty($data[country])){
+                    $data[country] = "не указанно";
+                }
+
+                if (!empty($_POST['hobbi'])){
+                    $hobbi = implode(',', $data[hobbi]);
+                    $data[hobbi] = $hobbi;
+                }else{
+                    $data[hobbi] = 'Не указанно';
+                }
+
+                if (empty($_POST[dop_inf])){
+                    $data[dop_inf] = 'Не указанно';
+                }
+                
+                $fp = fopen ('info_pers/bd.txt', 'a+');
+                foreach ($data as $key => $value) {
+                    fwrite ($fp, "$data[$key] ");
+                }
+
+                fwrite ($fp, PHP_EOL);
+                fclose ($fp);
+                echo "Запись произведена успешно!";
             }
+            
+            
         }
+
 
         $form = new Form();
         if (!empty($_POST)) {
             $form->Proverka();
-            $form->Vivod();
-            $form->Zapis();
+            if(empty($osibki)){
+                $form->Vivod();
+                $form->Zapis();
+            }else{
+                $form->osibki();
+            }
         }
+
 
     ?>
 
